920e3b88d16a1dc605c7993a354b599bf2532532
rav0007 <ravish.nfs@gmail.com>
2019-03-17 11:01:22 +0530

    added stuff

---
+++ Python/parsev2.py
@@ -0,0 +1,90 @@
+
+
+import alphaloc
+import locallocator
+import traversal
+import distcalc
+import netnodecalc
+import argparse
+import prioritizer
+
+
+
+
+
+def finalizer(file,mm):
+
+        """ returns a number node to node traversals and change in directions for a complete traversal of given list passed"""
+        habis = list()
+        i=0
+        while(file[i] != '\n'):
+            if( file[i] != ','):
+                if( file[i].isdigit()) :
+                    temp = int( file[i])
+                    if( file[i+1].isdigit()):
+                        temp = temp *10 + int(file[i+1]) ; i +=1 
+
+                    habis.append(temp)
+            i+=1
+
+
+
+
+        animals = list()
+        i=0 
+        while( file[i] != '#'):
+            if( file[i].isalpha()):
+                st = file[i]+file[i+1];
+
+                i +=2 
+                animals.append(st)
+            else:
+                i+=1
+
+
+
+#        animals,habis = prioritizer.prioritize(habis,animals,(0,0))
+
+
+        travel_list = list()
+        for i in range(len(animals)):
+            travel_list.append(animals[i]);travel_list.append(habis[i])
+
+
+
+        last = len(travel_list)
+
+        destination = netnodecalc.calc_min(travel_list[last-2],travel_list[last-1])
+
+
+
+        nodes = list()
+
+        for i in range(len(travel_list)):
+            if ( not  str(travel_list[i]).isdigit()):
+                nodes.append( alphaloc.finder(travel_list[i]) )
+            else:
+                nodes.append( netnodecalc.calc_min( travel_list[i-1], travel_list[i]))
+
+
+
+        s = (0,0)
+
+
+        dir = 90
+        tt= 0
+        count = 0 
+        turns = 0
+
+
+        for i in range(last):
+
+
+            d = nodes[i]
+            while(s  != d):
+                (s,dir,count,turns)  = traversal.travel(s,d,dir,count,turns)
+                if (count > mm):
+                     return(999999,0)
+        return(count,turns)
+
+
